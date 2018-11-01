2ce94fb271f1e666af9d170a5a5ed9a2befdeb9f
natakhatina <natakhatina@gmail.com>
2018-11-01 15:22:33 +0300

    task3.2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
 int main() {
-    double i = 10.3, t = 3.3, phi = 3.5, res;
-    res = pow(M_E, (-i)*t*(sqrt(phi))+phi*pow(M_E, ((sqrt(phi)/i*t))));
-    std::cout << res << '\n';
+    int a = 1;
+    unsigned char b = 129;
+    short c = -32768;
+    float d = 1000.5;
+    // std::cout<< a << ' ' << b << ' ' << c << ' ' << d << '\n'; //
     return 0;
 }
